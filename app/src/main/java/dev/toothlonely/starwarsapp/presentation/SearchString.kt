08947dd714a584077281b5661de3051201973488
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-03-30 01:01:01 +0300

    SWA-3 fixed search string look

--- app/src/main/java/dev/toothlonely/starwarsapp/presentation/SearchString.kt
+++ app/src/main/java/dev/toothlonely/starwarsapp/presentation/SearchString.kt
@@ -1,14 +1,12 @@
 package dev.toothlonely.starwarsapp.presentation
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -35,7 +33,6 @@ fun SearchString() {
         ),
         modifier = Modifier
             .fillMaxWidth()
-            .clip(RoundedCornerShape(30))
     )
 }
 
