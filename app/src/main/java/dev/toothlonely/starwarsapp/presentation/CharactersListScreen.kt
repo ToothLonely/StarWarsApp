b67a41d9d0434a3676a54bc5965d2600ae0ac6eb
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-03-29 16:17:19 +0300

    SWA-3 added CharactersListScreen

---
+++ app/src/main/java/dev/toothlonely/starwarsapp/presentation/CharactersListScreen.kt
@@ -0,0 +1,43 @@
+package dev.toothlonely.starwarsapp.presentation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.toothlonely.starwarsapp.data.STUB
+import dev.toothlonely.starwarsapp.domain.Character
+
+@Composable
+fun CharactersListScreen(listOfCharacters: List<Character>) {
+    LazyColumn {
+        itemsIndexed(listOfCharacters) { index, item ->
+            with(item) {
+                val firstLine = name
+                val secondLine = "$species, $gender,  $height cm"
+                val thirdLine = homeworld
+                Item(firstLine, secondLine, thirdLine)
+            }
+
+            if (index < listOfCharacters.size) {
+                HorizontalDivider(
+                    thickness = 1.dp,
+                    color = Color.LightGray,
+                    modifier = Modifier.padding(horizontal = 5.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun Preview() {
+    val listOfItems = STUB.getCharacters()
+
+    CharactersListScreen(listOfItems)
+}
--- app/src/main/java/dev/toothlonely/starwarsapp/presentation/MainScreen.kt
+++ app/src/main/java/dev/toothlonely/starwarsapp/presentation/MainScreen.kt
@@ -7,15 +7,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import dev.toothlonely.starwarsapp.data.STUB
 
 @Composable
 fun MainScreen(modifier: Modifier) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(50.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
         SearchString()
+
+        val listOfCharacters = STUB.getCharacters()
+        CharactersListScreen(listOfCharacters)
     }
 }
 
