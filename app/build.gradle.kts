b96a6a1bb2c7cca7e467ead29556da3816105f30
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-03-30 01:03:13 +0300

    SWA-4 include navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
 
     implementation(libs.kotlinx.serialization.json)
 
+    implementation(libs.navigation)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
