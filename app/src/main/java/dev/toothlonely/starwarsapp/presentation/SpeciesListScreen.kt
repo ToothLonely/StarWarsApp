96e6e7cd8872d0685302dc90f700770f978f3d32
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-03-29 18:49:36 +0300

    SWA-3 added SpeciesListScreen

---
+++ app/src/main/java/dev/toothlonely/starwarsapp/presentation/SpeciesListScreen.kt
@@ -0,0 +1,43 @@
+package dev.toothlonely.starwarsapp.presentation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.toothlonely.starwarsapp.data.STUB
+import dev.toothlonely.starwarsapp.domain.Species
+
+@Composable
+fun SpeciesListScreen(listOfSpecies: List<Species>) {
+    LazyColumn {
+        itemsIndexed(listOfSpecies) { index, item ->
+            with(item) {
+                val firstLine = name
+                val secondLine = "$classification from $homeworld"
+                val thirdLine = "Talking on $language"
+                Item(firstLine, secondLine, thirdLine)
+            }
+
+            if (index < listOfSpecies.size) {
+                HorizontalDivider(
+                    thickness = 1.dp,
+                    color = Color.LightGray,
+                    modifier = Modifier.padding(horizontal = 5.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun Preview() {
+    val listOfSpecies = STUB.getSpecies()
+
+    SpeciesListScreen(listOfSpecies)
+}
