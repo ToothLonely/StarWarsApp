a3b5f2f350400df22ebe56fc7096dbc0b14dce4a
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-03-29 16:42:49 +0300

    SWA-1 added Films

---
+++ app/src/main/java/dev/toothlonely/starwarsapp/domain/Film.kt
@@ -0,0 +1,10 @@
+package dev.toothlonely.starwarsapp.domain
+
+data class Film(
+    val title: String,
+    val releaseDate: String,
+    val openingCrawl: String,
+    val director: String,
+    val producer: String,
+    val episodeId: Int,
+)
